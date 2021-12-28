9b4900d9816b0ee1af0831661b08656392e168ad
EckoTc <84422175+EckoTc@users.noreply.github.com>
2021-12-28 16:27:30 -0600

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -10,7 +10,7 @@ $Action= New-ScheduledTaskAction -Execute "cmd.exe" -Argument "C:\Temp\PulseNetw
 Register-ScheduledTask -TaskName "PulseSecureInstall" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force # Specify the name of the task
 #>
 
-Invoke-Webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/Default.pulsepreconfig -outfile C:\Temp\Default.pulseconfig
+Invoke-Webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/Default.pulsepreconfig -outfile C:\Temp\Default.pulsepreconfig
 Invoke-Webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/PulseNetworkConnectCompiled.bat -outfile C:\Temp\PulseNetworkConnectCompiled.bat
 Invoke-WebRequest -Uri "https://github.com/EckoTc/Pulse/raw/main/PulseSecureInstallerService.msi" -OutFile "C:\Temp\PulseSecureInstallerService.msi"
 Invoke-WebRequest -Uri "https://statics.cirrus.com/vpn/PulseSecure-9.1R12.x64.msi" -OutFile "C:\Temp\ps-pulse-win-x64.msi"
