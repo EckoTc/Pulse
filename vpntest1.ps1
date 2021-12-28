485579d0af74573d976a94c3b62eaaa9057ff51d
EckoTc <84422175+EckoTc@users.noreply.github.com>
2021-12-28 11:29:00 -0600

    Update vpntest1.ps1

--- vpntest1.ps1
+++ vpntest1.ps1
@@ -9,8 +9,8 @@ $User= "NT AUTHORITY\SYSTEM" # Specify the account to run the script
 $Action= New-ScheduledTaskAction -Execute "cmd.exe" -Argument "C:\Temp\PulseNetworkConnectCompiled.bat" # Specify what program to run and with its parameters
 Register-ScheduledTask -TaskName "PulseSecureInstall" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force # Specify the name of the task
 
-Invoke-webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/Default.pulsepreconfig -outfile C:\Temp\Default.pulseconfig
-Invoke-webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/PulseNetworkConnectCompiled.bat -outfile C:\Temp\PulseNetworkConnectCompiled.bat
+Invoke-Webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/Default.pulsepreconfig -outfile C:\Temp\Default.pulseconfig
+Invoke-Webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/PulseNetworkConnectCompiled.bat -outfile C:\Temp\PulseNetworkConnectCompiled.bat
 
 
-Invoke-WebRequest -Uri "http://www.contoso.com" -OutFile "C:\path\file"
+Invoke-WebRequest -Uri "https://github.com/EckoTc/Pulse/raw/main/PulseSecureInstallerService.msi" -OutFile "C:\Temp\PulseSecureInstallerService.msi"
