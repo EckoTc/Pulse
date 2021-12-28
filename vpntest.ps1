0b7f2ca7a45546de7847841a8ef24e4172dfdb0b
EckoTc <84422175+EckoTc@users.noreply.github.com>
2021-12-28 11:07:03 -0600

    Create vpntest.ps1

---
+++ vpntest.ps1
@@ -0,0 +1,13 @@
+$path = "C:\temp\"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+$Trigger= New-ScheduledTaskTrigger -AtLogon # Specify the trigger settings
+$User= "NT AUTHORITY\SYSTEM" # Specify the account to run the script
+$Action= New-ScheduledTaskAction -Execute "cmd.exe" -Argument "C:\Temp\PulseNetworkConnectCompiled.bat" # Specify what program to run and with its parameters
+Register-ScheduledTask -TaskName "PulseSecureInstall" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force # Specify the name of the task
+
+Invoke-webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/Default.pulsepreconfig -outfile c:\temp\Default.pulseconfig
+Invoke-webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/PulseNetworkConnectCompiled.bat -outfile PulseNetworkConnectCompiled.bat
