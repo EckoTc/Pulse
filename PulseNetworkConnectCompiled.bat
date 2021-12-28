5891a2f3c904dad4fbc361cea5cf4d13d77df718
EckoTc <84422175+EckoTc@users.noreply.github.com>
2021-12-28 11:10:35 -0600

    Update PulseNetworkConnectCompiled.bat

--- PulseNetworkConnectCompiled.bat
+++ PulseNetworkConnectCompiled.bat
@@ -1,7 +1,7 @@
 "C:\Program Files (x86)\Pulse Secure\Network Connect 8.2\uninstall.exe" /silent=1
 "C:\Program Files (x86)\Pulse Secure\Network Connect 8.3\uninstall.exe" /silent=1
 "C:\Program Files (x86)\Pulse Secure\Secure Application Manager\UninstallSAM.exe" /qn
-msiexec -i "C:\Temp\ps-pulse-win-9.1r11.5-b9451-x64.msi" CONFIGFILE="C:\Temp\Default.pulsepreconfig" /l*v "C:\Temp\PulseSecure_Install.log" /q
+msiexec -i "C:\Temp\ps-pulse-win-x64.msi" CONFIGFILE="C:\Temp\Default.pulsepreconfig" /l*v "C:\Temp\PulseSecure_Install.log" /q
 PING 127.0.0.1 -n 1 -w 5000 >NUL
 taskkill /F /IM Pulse.exe 
 msiexec /i "C:\Temp\PulseSecureInstallerService.msi" /qn
