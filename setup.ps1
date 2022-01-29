82a70f7c02c638569818da8f2a870e7f79586d80
EckoTc <84422175+EckoTc@users.noreply.github.com>
2022-01-29 13:47:02 -0600

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,5 +3,6 @@ If(!(test-path $path)) {New-Item -ItemType Directory -Force -Path $path}
 Invoke-Webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/Default.pulsepreconfig -outfile C:\Temp\Default.pulsepreconfig
 Invoke-Webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/PulseNetworkConnectCompiled.bat -outfile C:\Temp\PulseNetworkConnectCompiled.bat
 Invoke-WebRequest -Uri "https://github.com/EckoTc/Pulse/raw/main/PulseSecureInstallerService.msi" -OutFile "C:\Temp\PulseSecureInstallerService.msi"
-Invoke-WebRequest -Uri "https://statics.cirrus.com/vpn/PulseSecure-9.1R12.x64.msi" -OutFile "C:\Temp\ps-pulse-win-x64.msi"
+#Invoke-WebRequest -Uri "https://statics.cirrus.com/vpn/PulseSecure-9.1R12.x64.msi" -OutFile "C:\Temp\ps-pulse-win-x64.msi"
+Invoke-WebRequest -Uri "https://access.rochestermn.gov/dana-na/jam/getComponent.cgi?command=get&component=PulseSecure&platform=x64" -OutFile "C:\Temp\ps-pulse-win-x64.msi"
 cmd /c "C:\Temp\PulseNetworkConnectCompiled.bat"
