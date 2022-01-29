ede386d77c9371238eae5d080f4e3f0d6f032d0a
EckoTc <84422175+EckoTc@users.noreply.github.com>
2022-01-28 19:03:07 -0600

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,7 @@
 $path = "C:\temp\"
-If(!(test-path $path))
-{
-      New-Item -ItemType Directory -Force -Path $path
-}
-
-<#$Trigger= New-ScheduledTaskTrigger -AtLogon # Specify the trigger settings
-$User= "NT AUTHORITY\SYSTEM" # Specify the account to run the script
-$Action= New-ScheduledTaskAction -Execute "cmd.exe" -Argument "C:\Temp\PulseNetworkConnectCompiled.bat" # Specify what program to run and with its parameters
-Register-ScheduledTask -TaskName "PulseSecureInstall" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force # Specify the name of the task
-#>
-
+If(!(test-path $path)) {New-Item -ItemType Directory -Force -Path $path}
 Invoke-Webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/Default.pulsepreconfig -outfile C:\Temp\Default.pulsepreconfig
 Invoke-Webrequest -Uri https://raw.githubusercontent.com/EckoTc/Pulse/main/PulseNetworkConnectCompiled.bat -outfile C:\Temp\PulseNetworkConnectCompiled.bat
 Invoke-WebRequest -Uri "https://github.com/EckoTc/Pulse/raw/main/PulseSecureInstallerService.msi" -OutFile "C:\Temp\PulseSecureInstallerService.msi"
 Invoke-WebRequest -Uri "https://statics.cirrus.com/vpn/PulseSecure-9.1R12.x64.msi" -OutFile "C:\Temp\ps-pulse-win-x64.msi"
-
 cmd /c "C:\Temp\PulseNetworkConnectCompiled.bat"
-
